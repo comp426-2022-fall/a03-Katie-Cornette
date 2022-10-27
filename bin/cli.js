4e79325a5b5398ed7fdd0e91fb3ca5c3f7d98f00
Katie Cornette <kathcorn@email.unc.edu>
2022-10-26 20:34:38 -0400

    Test Run

---
+++ bin/cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+
+import {roll} from "/lib/roll.js";  
+
+const args = minimist(process.argv.slice(2))
+
+const sides = args.sides ? args.dice: 6;
+const dice = args.dice ? args.dice: 2;
+const rolls = args.rolls ? args.rolls: 1;
+const results = roll(sides, dice, rolls);
+
+console.log(JSON.stringify(results)); 
+process.exit(0); 

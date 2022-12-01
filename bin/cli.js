449399b1cab959dfb800b2cab0ec3a48237983e3
Katie Cornette <kathcorn@email.unc.edu>
2022-12-01 09:37:07 -0500

    update roll.js

--- bin/cli.js
+++ bin/cli.js
@@ -6,10 +6,10 @@ import { roll } from "../lib/roll.js";
 
 const args = minimist(process.argv.slice(2))
 
-const sides = args.sides ? args.dice: 6;
-const dice = args.dice ? args.dice: 2;
-const rolls = args.rolls ? args.rolls: 1;
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
 const results = roll(sides, dice, rolls);
 
-console.log(JSON.stringify(results)); 
+console.log(results); 
 process.exit(0); 
